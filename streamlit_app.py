845bd5e44fdf5eea220e64a8eb7012bbb8f31ad4
dcarrascop <68603716+dcarrascop@users.noreply.github.com>
2024-09-23 20:01:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,24 @@
+import gdown
 from sqlalchemy import create_engine, text
 import streamlit as st
 import pandas as pd
+import os
 
-# Crear un motor a una base de datos de prueba
-engine = create_engine('sqlite:///chinook.db')  # Archivo de base de datos de ejemplo
+# Descargar el archivo desde Google Drive si no existe
+db_file = 'licencias.db'
+if not os.path.exists(db_file):
+    url = 'https://drive.google.com/uc?id=17bXAOXt6nsP0Ch8KwZISiFK6vN4rvopV'  # Reemplaza con tu enlace de Google Drive
+    gdown.download(url, db_file, quiet=False)
 
-# Ejecutar una consulta básica y mostrar los primeros 5 registros de una tabla existente
+# Cargar la base de datos
+engine = create_engine(f'sqlite:///{db_file}')
+
+# Ejecutar una consulta básica y mostrar los primeros 5 registros
 with engine.connect() as connection:
-    result = connection.execute(text("SELECT * FROM albums LIMIT 5"))
-    df = pd.DataFrame(result.fetchall(), columns=result.keys())
-    st.write(df)
+    try:
+        result = connection.execute(text("SELECT * FROM licencias LIMIT 5"))
+        # Convertir el resultado a un DataFrame para mostrarlo en Streamlit
+        df = pd.DataFrame(result.fetchall(), columns=result.keys())
+        st.write(df)
+    except Exception as e:
+        st.error(f"Error al ejecutar la consulta: {e}")
