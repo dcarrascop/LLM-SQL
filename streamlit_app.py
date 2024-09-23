9524732dee16547da899c4e885d18dc541060197
dcarrascop <68603716+dcarrascop@users.noreply.github.com>
2024-09-23 19:54:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,15 +10,9 @@ if not os.path.exists(db_file):
     url = 'https://drive.google.com/uc?id=17bXAOXt6nsP0Ch8KwZISiFK6vN4rvopV'
     gdown.download(url, db_file, quiet=False)
 
-# Cargar la base de datos
-engine = create_engine(f'sqlite:///{db_file}')
+import os
 
-# Ejecutar una consulta básica y mostrar los primeros 5 registros
-with engine.connect() as connection:
-    try:
-        result = connection.execute(text("SELECT * FROM licencias LIMIT 5"))
-        # Convertir el resultado a un DataFrame para mostrarlo en Streamlit
-        df = pd.DataFrame(result.fetchall(), columns=result.keys())
-        st.write(df)
-    except Exception as e:
-        st.error(f"Error al ejecutar la consulta: {e}")
+if os.path.exists(db_file):
+    st.write(f"Tamaño del archivo: {os.path.getsize(db_file)} bytes")
+else:
+    st.error("El archivo no existe.")
